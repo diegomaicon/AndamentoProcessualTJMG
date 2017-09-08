a4bc3c506b152779b5a8acb22eec1f805ac91430
Diego Maicon Silva <diego_maicon20@hotmail.com>
2017-09-07 23:12:02 -0300

    Criada classe processo

---
+++ app/src/main/java/com/example/diego/andamentoprocessualtjmg/ConexaoTJMG.java
@@ -0,0 +1,18 @@
+package com.example.diego.andamentoprocessualtjmg;
+
+import android.text.Html;
+
+import java.io.InputStream;
+
+/**
+ * Created by diego on 07/09/17.
+ */
+
+public class ConexaoTJMG {
+
+
+    private static final String URL = "http://www4.tjmg.jus.br/juridico/sf/proc_resultado.jsp?tipoPesquisa=1&txtProcesso=0261150034831&comrCodigo=261&nomePessoa=&tipoPessoa=X&naturezaProcesso=0&situacaoParte=X&codigoOAB=&tipoOAB=N&ufOAB=MG&numero=1&select=1&tipoConsulta=1&natureza=0&ativoBaixado=X&listaProcessos=15003483";
+
+
+
+}
